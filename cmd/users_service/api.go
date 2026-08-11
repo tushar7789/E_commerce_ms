8cabdb3f2a5b2f65d1c75827fe97f4b40220098d
tushar7789 <tusharjangid7@gmail.com>
2026-08-11 15:09:58 +0530

    [changed] names of microservices from - to _

---
+++ cmd/users_service/api.go
@@ -0,0 +1,21 @@
+package users_service
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
--- internal/domain/users.go
+++ internal/domain/users.go
@@ -1,1 +1,20 @@
 package domain
+
+import (
+	"database/sql"
+)
+
+type application struct {
+	config config
+	//logger
+	db *sql.DB
+}
+
+type config struct {
+	addr string // addr of server
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string // user, pass, domain
+}
